e6b0ef7fabb103596fdf966868cab4c380e11b0b
Gustavo <gustavohenriquealves418@gmail.com>
2025-07-28 18:44:36 -0300

    Arrumando

---
+++ algoritmo_tecnicas_programacao/Algoritmos/diijkstra.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+
+using namespace std;
+
+/*
+Em um cenário urbano em constante evolução, considere uma rede de transporte inteligente (ITS) que monitora o tráfego em tempo real, acidentes
+,obras e eventos especiais. O objetivo é fornecer aos motoristas a rota mais rápida do ponto A ao ponto B, considerando que os pesos das 
+arestas (tempos de viagem) mudam constantemente devido às condições do tráfego.
+ */
+
+
+int main(){
+    return 0;
+
+}
