f56ec3c68343d5508d441ba76850edffcc57a5ed
Gustavo <gustavohenriquealves418@gmail.com>
2025-10-17 23:55:27 -0300

    Consertando a geração de produtos

--- algoritmo_estrutura_dados/projeto_ordenar.py
+++ algoritmo_estrutura_dados/projeto_ordenar.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+import datetime
 import random 
 
 
 class Produto:
-    def __init__(self,produto:str,preco:int,avaliacao:float,data_adicao:datetime,categoria:str):       
+    def __init__(self,produto:str,preco:int,avaliacao:float,data_adicao:datetime.datetime,categoria:str):       
         self.produto = produto 
         self.preco = preco 
         self.avaliacao = avaliacao 
@@ -13,17 +13,22 @@ class Produto:
     def __repr__(self):
         return f"{self.produto}: {self.preco}, {self.avaliacao}, {self.data_adicao}, {self.categoria}"
    
+
 def gerando_dados(n):
     nome = ["Produtos " + str(c) for c in range(n)]
     preco = [round(random.uniform(0,1000),2) for c in range(n)]
     avaliacao = [round(random.uniform(0,5),2) for _ in range(n)]
     datas = [datetime.datetime.now() - datetime.timedelta(days=random.randint(0, 365)) for _ in range(n)]
-    categoria = ["Categoria: " + str(random.radint(0,5)) for c in range(n)]
-    produto = [Produto(nome[i], preco[i], avaliacao[i],datas[i],categoria[i]) for i in range(n)]
+    categoria = ["Categoria: " + str(random.randint(0,5)) for c in range(n)]
+    produto = [Produto(datas[i],nome[i], preco[i], avaliacao[i],categoria[i]) for i in range(n)]
     return produto
 
 
-
 produtos = gerando_dados(1000)
-for produto in produtos[:10:]:
+for produto in produtos:
     print(produto)
+
+
+
+def buble_sort():
+    pass 
